70687a92074b19958109f3ba914d2e060b3885b4
DemoAnge <angelpilamunga2098@gmail.com>
2025-01-06 15:45:52 -0500

    cambi de logo

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,7 +8,7 @@ import mprocess from "../assets/imgproyects/doc.jpeg"
 const projects_data =[
     {
     title: "CNN Clothing Color Recognition",
-    description: "I developed a convolutional neural network to train it for recognizing individuals and the main clothing items they were wearing, identifying the colors of both upper and lower garments. Libraries that facilitated the project's development were utilized. This program includes an interface for real-time clothing color detection using the device’s camera.",
+    description: "Developmen a convolutional neural network to train it for recognizing individuals and the main clothing items they were wearing, identifying the colors of both upper and lower garments. Libraries that facilitated the project's development were utilized. This program includes an interface for real-time clothing color detection using the device’s camera.",
     participation: "Building the convolutional neural network using the YOLO library, training and testing the model and developing the interface to use the system.",
     technologies: ["Python"],
     img: [network, network2],
@@ -16,7 +16,7 @@ const projects_data =[
     },
     {
     title: "InvControl (Inventory Management",
-    description: "I designed an inventory management system to control non-current assets of the Faculty of Systems, Electronics, and Industrial Engineering at the Technical University of Ambato. This project included registration of categories, locations, and assets, asset deletion and control and detailed report generation.",
+    description: "Developmen an inventory management system to control non-current assets of the Faculty of Systems, Electronics, and Industrial Engineering at the Technical University of Ambato. This project included registration of categories, locations, and assets, asset deletion and control and detailed report generation.",
     participation: "I served as the project manager and was responsible for FrontEnd development of the modules.",
     technologies: ["Angular","Tailwind","Python","MySQL"],
     img: [inv1,inv2,inv3],
@@ -24,7 +24,7 @@ const projects_data =[
     },
     {
         title: "Document Process Map",
-        description: "I developed a web application for the company “Mushuc Runa Ltda.” to control document management and access restrictions across different departments.",
+        description: "Development support of a web application for the company “Mushuc Runa Ltda.” to control document management and access restrictions in the different departments.",
         participation: "I contributed to the FrontEnd development of some application modules.",
         technologies: ["Angular" ],
         img: [mprocess],
